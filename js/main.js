d22f2094d37e3db21ee42d63aa05d94a6b1c7bc2
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-05-04 16:54:17 +0200

    working in progress

--- js/main.js
+++ js/main.js
@@ -55,3 +55,56 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+// funzione che mi servirà per cambiare i likes
+function footer(item, box) {
+    let footer = document.createElement('div')
+    footer.setAttribute('class', 'post_footer')
+    footer.innerHTML = ''
+    footer.innerHTML = `
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter"> Piace a <b id="like-counter-${item.id}" class="js-likes-counter">${item.likes}</b> persone</div>
+        </div> 
+    `
+    box.append(footer)
+}
+
+// funzione di creazione dei post 
+function createPost() {
+    const container = document.getElementById('container')
+    posts.forEach((item) => {
+        let box = document.createElement('div')
+        box.setAttribute('class', 'post')
+        let top = document.createElement('div')
+        top.setAttribute('class', 'post_header')
+        top.innerHTML = `
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${item.author.image}" alt="${item.author.name}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${item.author.name}</div>
+                    <div class="post-meta__time">${item.created}</div>
+                </div>                    
+            </div>
+        `
+        let text = document.createElement('div')
+        text.setAttribute('class', 'post_text')
+        text.innerHTML = `${item.content}`
+        let img = document.createElement('div')
+        img.setAttribute('class', 'post__image')
+        img.innerHTML = `<img src="${item.media}" alt="${item.author.name}">`
+
+        container.append(box)
+        box.append(top)
+        box.append(text)
+        box.append(img)
+        footer(item, box)
+    })
+}
+createPost()
